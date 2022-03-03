3619f0f966859babb6368928b37132d0d84e8d98
VanshitaGarg16 <vanshita.garg@lumiq.ai>
2022-03-03 12:57:21 +0530

    Added Comments

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const mongoose = require("mongoose")
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 
+
+//Connection Request
 const db = mongoose.connection
 db.on("error", (error) => console.error(error))
 db.once("open", () => console.log("Connected to Database"))
@@ -16,4 +18,5 @@ app.use(express.json())
 const subscribersRouter = require("./routes/subscribers")
 app.use("/subscribers", subscribersRouter)
 
+//Port setting to 3000
 app.listen(3000, () => console.log("Port is Listening"))
